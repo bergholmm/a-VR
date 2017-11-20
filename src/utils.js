6c692a56b84ffd82594de04d0e0380af268d8f69
Petrenko96 <thomas.marissal96@gmail.com>
2017-11-20 12:49:19 +0100

    No more two items with the same shape and color

--- src/utils.js
+++ src/utils.js
@@ -4,6 +4,20 @@ import Item from './Item';
 export const itemPositions = ['-1.3 2.8 -3.9', '0 2.8 -3.9', '1.3 2.8 -3.9', '-1.3 1.3 -3.9', '0 1.3 -3.9', '1.3 1.3 -3.9', '-1.3 0.3 -3.9', '0 0.3 -3.9', '1.3 0.3 -3.9'];
 export const itemTypes = ['a-box', 'a-sphere', 'a-cylinder'];
 export const itemColors = ['#4CC3D9', '#EF2D5E', '#FFC65D', '#7BC8A4', '#ca96fd', '#fdfd96'];
+export const itemColors2 = ['#03402b',
+'#9f3814',
+'#43f6db',
+'#8d2547',
+'#b10fe1',
+'#8f05dc',
+'#b5b1b2',
+'#7ff6ab',
+'#b2f5aa',
+'#e024ef',
+'#eb516d',
+'#81d3a4',
+'#c23009']
+
 
 export const shuffleArray = (array) => {
     for (var i = array.length - 1; i > 0; i--) {
@@ -27,19 +41,25 @@ export const generateItemList = (props, length = 9) => {
     let items = [];
     for (let i = 0; i < length; ++i) {
 
-        let y = itemTypes[Math.floor(Math.random()*itemTypes.length)];
+        let typ = itemTypes[Math.floor(Math.random()*itemTypes.length)];
+        let col =  itemColors2[Math.floor(Math.random()*itemColors2.length)];
+
+        while(items.filter(e => e.props.primitive === typ && e.props.color === col).length > 0) {
+          typ = itemTypes[Math.floor(Math.random()*itemTypes.length)];
+          col =  itemColors2[Math.floor(Math.random()*itemColors2.length)];
+        }
 
         items.push(
             <Item
                 key={ i }
                 id={ i }
-                primitive={ y }
+                primitive={ typ }
                 position={ itemPositions[i] }
                 radius='0.3'
-                height={ ((y === 'a-sphere') ? null : '0.5' )}
-                width={  ((y === 'a-box') ? '0.5' : null) }
-                depth={ ((y === 'a-box') ? '0.5' : null)}
-                color={ itemColors[Math.floor(Math.random()*itemColors.length)] }
+                height={ ((typ === 'a-sphere') ? null : '0.5' )}
+                width={  ((typ === 'a-box') ? '0.5' : null) }
+                depth={ ((typ === 'a-box') ? '0.5' : null)}
+                color={ col }
                 remove={ props.removeEntity }
                 getNext={ props.getNextItem }
             />,
