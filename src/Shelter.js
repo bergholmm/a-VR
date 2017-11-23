9b8682d389e234b78bd571880c55e1a68e457216
Petrenko96 <thomas.marissal96@gmail.com>
2017-11-23 15:19:38 +0100

    Working and proper scaling shelter

---
+++ src/Shelter.js
@@ -0,0 +1,141 @@
+import 'aframe';
+import 'aframe-animation-component';
+import 'aframe-particle-system-component';
+import 'babel-polyfill';
+import { Entity } from 'aframe-react';
+import React from 'react';
+import Item from './Item'
+import Chose from './chose'
+import {
+    createIndexArray,
+    shuffleArray,
+    generateItemList
+} from './utils';
+
+class Shelter extends Chose<Item> {
+
+  constructor(props) {
+    super(props);
+
+    this.width = props.width;
+    this.height = props.height;
+    this.coeff = 1.333;
+
+
+    let distance = 4.5;
+    let depth = 1;
+    let width = this.width*this.coeff;
+    let height = this.height;
+
+    let queue = createIndexArray(this.width*this.height);
+    queue = shuffleArray(queue);
+
+    let itemProps = {
+        removeEntity: this.removeEntity.bind(this),
+        getNextItem: this.getNext.bind(this),
+    }
+
+    let positions = this.positionList();
+
+    let listItems = generateItemList(itemProps, positions, this.width*this.height);
+
+    let pos = [];
+    for(let i = 0 ; i <= this.height ; i++) {
+      pos.push(<Entity key={i} primitive='a-plane' position={`0 ${i} ${-distance+depth/2}`} rotation={`${(i < 2) ? -90 : 90} 0 90`}
+        width={depth} height={width} color={(i > 0 && i < height) ? 'black' : 'brown'} />);
+    }
+    this.shelter = (
+      <Entity>
+      <Entity primitive='a-plane' position={`0 ${height/2} ${-distance}`} rotation='0 0 0' width={width} height={height} color='brown' />
+      <Entity primitive='a-plane' position={`${width/2} ${height/2} ${-distance+depth/2}`} rotation='0 -90 0' width={depth} height={height} color='brown' />
+      <Entity primitive='a-plane' position={`${-width/2} ${height/2} ${-distance+depth/2}`} rotation='0 90 0' width={depth} height={height} color='brown' />
+      {pos}
+      </Entity>
+    );
+
+    this.positionNext = ((width/2)+0.3) + ' 1.3 -3';
+    this.positionText = {
+      x: ((width/2)+0.3),
+      y: 2,
+      z: -3
+    }
+
+    this.state = {
+      queue: queue,
+      items: listItems
+    }
+  }
+
+  positionList() {
+    let a = [];
+    for(let i = this.height-1 ; i >= 0 ; i--) {
+      for(let j = 0 ; j < this.width ; j++) {
+        a.push((j-((this.width-1)/2))*this.coeff + ' ' + (i+0.3) + ' -3.9');
+      }
+    }
+
+    return a;
+  }
+
+  removeEntity(id) {
+      let items = this.state.items;
+      let queue = this.state.queue;
+      items[id] = null;
+      queue.shift();
+      console.log(items, queue);
+
+      this.setState( {
+        items: items,
+        queue: queue
+      });
+  }
+
+  getNext() {
+    return this.state.queue[0];
+  }
+
+  hasNext() {
+    return this.state.queue.length > 0;
+  }
+
+  render() {
+    let items = this.state.items;
+    let nextItem = {};
+    if(!this.hasNext()) {
+      nextItem = <Item
+              primitive='a-sphere'
+              position={this.positionNext}
+              radius='0.3'
+              color='grey'
+              remove={ () => {} }
+              getNext={ () => {} }
+          />;
+    } else {
+      let {items, queue} = this.state;
+      let x = items[queue[0]].props.color;
+      nextItem = <Item
+              primitive={ items[queue[0]].props.primitive }
+              width={ items[queue[0]].props.width }
+              depth={ items[queue[0]].props.depth }
+              height={ items[queue[0]].props.height}
+              position={this.positionNext}
+              radius='0.3'
+              color={ x }
+              remove={ () => {} }
+              getNext={ () => {} }
+          />
+    }
+    return (
+      <Entity>
+      {items}
+      {this.shelter}
+      <Entity text={{value: 'Next:', align: 'center', width: 8}} position={this.positionText}/>
+      {nextItem}
+      </Entity>
+    );
+  }
+
+
+}
+
+export default Shelter;
---
+++ src/chose.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Chose<T> extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  hasNext() {}
+  getNext() {}
+  render() {}
+}
+
+export default Chose;
